8f5509130efc76ff48cedf01893a5c3a519cea1b
fxs‘s father about all part <40730793+ZJiaqi1@users.noreply.github.com>
2023-08-04 00:53:44 +0100

    这个部分用于评估基于内容的推荐方法

--- Recsys_work/dlHashedAnalyse.py
+++ Recsys_work/dlHashedAnalyse.py
@@ -8,24 +8,43 @@ import mysql
 
 # Load the dataset
 db = mysql.connect()
-sql='''
+sql = '''
 select * from dl_hash
 '''
 data = pd.read_sql(sql,db)
+sqlTrain='''
+select * from train_set
+'''
+data_train = pd.read_sql(sqlTrain,db)
+sqlTest = '''
+select * from test_set
+'''
+data_test = pd.read_sql(sqlTest,db)
 
 # Basic information about the dataset
 info = {
-    'Number of rows': data.shape[0],
-    'Number of columns': data.shape[1],
-    'Number of unique user_ids': data['user_id'].nunique(),
-    'Number of unique resource_ids': data['resource_id'].nunique(),
-    'First date': data['datetime'].min(),
-    'Last date': data['datetime'].max(),
+    'Number of rows': data_train.shape[0],
+    'Number of columns': data_train.shape[1],
+    'Number of unique user_ids': data_train['user_id'].nunique(),
+    'Number of unique resource_ids': data_train['resource_id'].nunique(),
+    'First date': data_train['datetime'].min(),
+    'Last date': data_train['datetime'].max(),
+}
+
+print(info)
+info = {
+    'Number of rows': data_test.shape[0],
+    'Number of columns': data_test.shape[1],
+    'Number of unique user_ids': data_test['user_id'].nunique(),
+    'Number of unique resource_ids': data_test['resource_id'].nunique(),
+    'First date': data_test['datetime'].min(),
+    'Last date': data_test['datetime'].max(),
 }
 
 print(info)
 
 # Convert datetime to datetime object
+data['datetime'] = pd.to_datetime(data['datetime'])
 data['datetime'] = pd.to_datetime(data['datetime'])
 
 # User access count
@@ -64,35 +83,44 @@ plt.show()
 
 
 # Due to limited computational resources, we will only use a subset of the data
-data_small = data.sample(frac=0.01, random_state=1)
+# data_small = data_train.sample(frac=0.01, random_state=1)
 
 # Convert user_id and resource_id to categorical variables
-data_small['user_id'] = data_small['user_id'].astype('category')
-data_small['resource_id'] = data_small['resource_id'].astype('category')
-
+data_train['user_id'] = data_train['user_id'].astype('category')
+data_train['resource_id'] = data_train['resource_id'].astype('category')
+data_test['user_id'] = data_test['user_id'].astype('category')
+data_test['resource_id'] = data_test['resource_id'].astype('category')
+data['user_id'] = data['user_id'].astype('category')
+data['resource_id'] = data['resource_id'].astype('category')
 # Create a user-resource matrix
-user_resource_matrix = sparse.coo_matrix((np.ones(len(data_small)),
-                                          (data_small['user_id'].cat.codes,
-                                           data_small['resource_id'].cat.codes)))
+user_resource_matrix = sparse.coo_matrix((np.ones(len(data)),
+                                          (data['user_id'].cat.codes,
+                                           data['resource_id'].cat.codes)))
 
 # Compute the similarity matrix
 similarity_matrix = cosine_similarity(user_resource_matrix)
 
 # Split the data into training set and test set
-train_data, test_data = train_test_split(data_small, test_size=0.2, random_state=1)
+# train_data, test_data = train_test_split(data_small, test_size=0.2, random_state=1)
 
-similarity_matrix.shape, train_data.shape, test_data.shape
+similarity_matrix.shape, data_train.shape, data_test.shape
 
 def recommend(user_id, similarity_matrix, data, top_k=5):
     """
+    为给定用户推荐资源
+     参数：
+     user_id：目标用户的id。
+     相似度矩阵：用户-用户相似度矩阵。
+     data：用户资源交互数据。
+     top_k：推荐的资源数量。
+     返回：
+     推荐的资源 ID 列表。
     Recommend resources for a given user.
-
     Parameters:
     user_id: The id of the target user.
     similarity_matrix: The user-user similarity matrix.
     data: The user-resource interaction data.
     top_k: The number of resources to recommend.
-
     Returns:
     A list of recommended resource ids.
     """
@@ -107,17 +135,17 @@ def recommend(user_id, similarity_matrix, data, top_k=5):
 
 
 # Test the recommend function
-test_user = test_data['user_id'].iloc[0]
-rec_resources = recommend(test_user, similarity_matrix, train_data)
+test_user = data_test['user_id'].iloc[0]
+rec_resources = recommend(test_user, similarity_matrix, data_test)
 rec_resources
 
 # Evaluate the recommendation system on the test set
 hits = 0
 total = 0
 
-for user_id in test_data['user_id'].unique():
-    actual_resources = test_data[test_data['user_id'] == user_id]['resource_id'].tolist()
-    rec_resources = recommend(user_id, similarity_matrix, train_data)
+for user_id in data_test['user_id'].unique():
+    actual_resources = data_test[data_test['user_id'] == user_id]['resource_id'].tolist()
+    rec_resources = recommend(user_id, similarity_matrix, data_test)
     hits += len(set(actual_resources) & set(rec_resources))
     total += len(rec_resources)
 
@@ -127,7 +155,7 @@ accuracy1
 
 
 # Compute the popularity of each resource
-resource_popularity = train_data['resource_id'].value_counts()
+resource_popularity = data_test['resource_id'].value_counts()
 
 # Select the top 5 most popular resources as the recommendations
 top_resources = resource_popularity.index[:5].tolist()
@@ -136,18 +164,14 @@ top_resources = resource_popularity.index[:5].tolist()
 hits = 0
 total = 0
 
-for user_id in test_data['user_id'].unique():
-    actual_resources = test_data[test_data['user_id'] == user_id]['resource_id'].tolist()
+for user_id in data_test['user_id'].unique():
+    actual_resources = data_test[data_test['user_id'] == user_id]['resource_id'].tolist()
     hits += len(set(actual_resources) & set(top_resources))
     total += len(top_resources)
 
 accuracy2 = hits / total
 #基于流行度的推荐系统结果
 accuracy2
-
-
-
-
 
 # Define the time slots
 time_slots = {0: 'morning', 1: 'morning', 2: 'morning', 3: 'morning', 4: 'morning', 5: 'morning',
@@ -156,8 +180,8 @@ time_slots = {0: 'morning', 1: 'morning', 2: 'morning', 3: 'morning', 4: 'mornin
               18: 'evening', 19: 'evening', 20: 'evening', 21: 'evening', 22: 'evening', 23: 'evening'}
 
 # Compute the popularity of each resource in each time slot
-train_data['time_slot'] = train_data['hour'].map(time_slots)
-resource_popularity = train_data.groupby('time_slot')['resource_id'].value_counts()
+data_test['time_slot'] = data_test['hour'].map(time_slots)
+resource_popularity = data_test.groupby('time_slot')['resource_id'].value_counts()
 
 # Define a function to recommend resources based on time
 def recommend_time(user_id, time_slot, resource_popularity, top_k=5):
@@ -177,10 +201,10 @@ def recommend_time(user_id, time_slot, resource_popularity, top_k=5):
     return top_resources
 
 # Add 'time_slot' to the test data
-test_data['time_slot'] = test_data['hour'].map(time_slots)
+data_test['time_slot'] = data_test['hour'].map(time_slots)
 
 # Test the recommend function again
-test_time_slot = test_data[test_data['user_id'] == test_user]['time_slot'].iloc[0]
+test_time_slot = data_test[data_test['user_id'] == test_user]['time_slot'].iloc[0]
 rec_resources = recommend_time(test_user, test_time_slot, resource_popularity)
 rec_resources
 
@@ -188,9 +212,9 @@ rec_resources
 hits = 0
 total = 0
 
-for user_id in test_data['user_id'].unique():
-    actual_resources = test_data[test_data['user_id'] == user_id]['resource_id'].tolist()
-    time_slot = test_data[test_data['user_id'] == user_id]['time_slot'].iloc[0]
+for user_id in data_test['user_id'].unique():
+    actual_resources = data_test[data_test['user_id'] == user_id]['resource_id'].tolist()
+    time_slot = data_test[data_test['user_id'] == user_id]['time_slot'].iloc[0]
     rec_resources = recommend_time(user_id, time_slot, resource_popularity)
     hits += len(set(actual_resources) & set(rec_resources))
     total += len(rec_resources)
